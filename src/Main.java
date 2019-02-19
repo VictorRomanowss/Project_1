209e99912b4bfe1c5a383c07b248bffb41dd40fd
Виктор Романов <romanow.vitya2018@icloud.com>
2019-02-19 19:51:18 +0300

    first 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
+        String a = "Hello";
 
+        System.out.println(a + " world");
     }
-}
+        }
+
+
+
+
+
